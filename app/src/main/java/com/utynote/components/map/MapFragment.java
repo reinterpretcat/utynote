8e0b975d18b244cff1c057cf5352977424bb7de3
Ilya Builuk <ilya.builuk@gmail.com>
2017-05-01 23:06:40 +0200

    Add google maps as temporary map provider

--- app/src/main/java/com/utynote/components/map/MapFragment.java
+++ app/src/main/java/com/utynote/components/map/MapFragment.java
@@ -1,5 +1,6 @@
 package com.utynote.components.map;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -9,15 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.utynote.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     public static final String TAG = MapFragment.class.getSimpleName();
+
+    private GoogleMap mMap;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle bundle) {
-
         View view = inflater.inflate(R.layout.map_container_view, container, false);
 
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
@@ -28,7 +35,26 @@ public class MapFragment extends Fragment {
                         .setAction("Action", null).show();
             }
         });
-
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        com.google.android.gms.maps.SupportMapFragment mapFragment = new com.google.android.gms.maps.SupportMapFragment();
+        mapFragment.getMapAsync(this);
+        getChildFragmentManager()
+                .beginTransaction()
+                .add(R.id.map_container, mapFragment)
+                .commit();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(52.53174, 13.38771);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18));
     }
 }
