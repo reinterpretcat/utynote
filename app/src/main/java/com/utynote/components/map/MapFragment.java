d16fef42270af92271093510bff6c46716f80e51
Ilya Builuk <ilya.builuk@gmail.com>
2017-05-01 14:42:28 +0200

    Move my location button to map fragment

--- app/src/main/java/com/utynote/components/map/MapFragment.java
+++ app/src/main/java/com/utynote/components/map/MapFragment.java
@@ -2,6 +2,8 @@ package com.utynote.components.map;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,18 @@ public class MapFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle bundle) {
-        return inflater.inflate(R.layout.map_container_view, container, false);
+
+        View view = inflater.inflate(R.layout.map_container_view, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Searching your location..", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        return view;
     }
 }
