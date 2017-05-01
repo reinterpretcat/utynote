6ed57f8db110544f0025b605a178e1aa60449d3b
Ilya Builuk <ilya.builuk@gmail.com>
2017-05-01 23:27:06 +0200

    Update comment

--- app/src/main/java/com/utynote/app/RootView.java
+++ app/src/main/java/com/utynote/app/RootView.java
@@ -7,7 +7,9 @@ import android.support.v4.widget.DrawerLayout;
 import com.utynote.components.search.SearchFragment;
 import com.utynote.widgets.panel.SlidingUpPanelLayout;
 
-/** Specifies behavior of application root view. */
+/**
+ * Specifies behavior of application root view which knows about all included components.
+ **/
 public interface RootView extends SearchFragment.Controller {
     @NonNull
     DrawerLayout getDrawerLayout();
