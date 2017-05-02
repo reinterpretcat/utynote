b44e45ceb6b32231951dbfdec11d575ced190b56
Ilya Builuk <ilya.builuk@gmail.com>
2017-05-02 21:50:12 +0200

    Move appbar logic to activity

--- app/src/main/java/com/utynote/activities/MainActivity.java
+++ app/src/main/java/com/utynote/activities/MainActivity.java
@@ -6,20 +6,21 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.utynote.R;
-import com.utynote.app.RootView;
 import com.utynote.components.map.MapFragment;
 import com.utynote.components.nearby.NearbyFragment;
 import com.utynote.components.search.SearchFragment;
 import com.utynote.widgets.panel.SlidingUpPanelLayout;
 
-public class MainActivity extends AppCompatActivity implements RootView,
-        NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private NavigationView mNavigationView;
     private SlidingUpPanelLayout mSlidingPanel;
     private DrawerLayout mDrawerLayout;
 
@@ -30,8 +31,22 @@ public class MainActivity extends AppCompatActivity implements RootView,
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mSlidingPanel = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
         mNavigationView.setNavigationItemSelectedListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                (Toolbar) findViewById(R.id.toolbar),
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
 
         getSupportFragmentManager()
                 .beginTransaction()
@@ -43,8 +58,8 @@ public class MainActivity extends AppCompatActivity implements RootView,
 
     @Override
     public void onBackPressed() {
-        if (getDrawerLayout().isDrawerOpen(GravityCompat.START)) {
-            getDrawerLayout().closeDrawer(GravityCompat.START);
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -52,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements RootView,
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
         if (id == R.id.nav_notes) {
@@ -67,25 +82,24 @@ public class MainActivity extends AppCompatActivity implements RootView,
 
         }
 
-        getDrawerLayout().closeDrawer(GravityCompat.START);
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    @NonNull
     @Override
-    public DrawerLayout getDrawerLayout() {
-        return mDrawerLayout;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_appbar, menu);
+        return true;
     }
 
-    @NonNull
     @Override
-    public NavigationView getNavigationView() {
-        return mNavigationView;
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-    @NonNull
-    @Override
-    public SlidingUpPanelLayout getSlidingPanel() {
-        return mSlidingPanel;
+        if (id == R.id.action_search) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/utynote/components/search/SearchFragment.java
+++ app/src/main/java/com/utynote/components/search/SearchFragment.java
@@ -1,73 +1,18 @@
 package com.utynote.components.search;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.utynote.R;
 
 public class SearchFragment extends Fragment {
     public static final String TAG = SearchFragment.class.getSimpleName();
 
-    public interface Controller {
-        @NonNull
-        DrawerLayout getDrawerLayout();
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle bundle) {
-        View view = inflater.inflate(R.layout.search_container_view, container, false);
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        toolbar.setTitle("Search");
-
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-
-        setHasOptionsMenu(true);
-
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        DrawerLayout drawer = ((Controller) getActivity()).getDrawerLayout();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                getActivity(),
-                drawer,
-                (Toolbar) view.findViewById(R.id.toolbar),
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.search_appbar, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_search) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return null;
     }
 }
